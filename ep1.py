a5a1a29ed38151a972fe7009a870361c8fd73dae
Lucas <lucasofa808@gmail.com>
2020-04-02 19:33:47 -0300

    Função plb e loop point

--- ep1.py
+++ ep1.py
@@ -10,3 +10,39 @@ def apostar():
         return True
     else:
         return False
+
+'''funções para modos de jogo'''
+def plb():
+    global fichas 
+    global z
+    global apostap
+    print("o número sorteado é:",z)
+    if(z== 7 or z== 11):
+        fichas=fichas+apostap 
+        print("Agora você tem o total de: {}".format(fichas))
+    elif(z== 2 or z== 3 or z== 12):
+        fichas=fichas-apostap
+        print("Agora você tem o total de: {}".format(fichas))
+    elif(z== 4 or z== 5 or z== 6 or z== 8 or z== 9 or z== 10):   
+        def loop_point():
+            w=sorteio()
+            global n
+            global fichas
+            print("Agora você se encontra na fase point referente ao modo plb pela {}° vez\n".format(n))
+            print("O valor {} retirado no priemiro sorteio é seu point".format(z))
+            print("O novo valor sorteado é",w)
+            n=n+1
+            print("agora você possui{}fichas",format(fichas))
+            if(w==z):
+                fichas=fichas+apostap
+                print(fichas)
+                return True
+            elif(w==7):
+                fichas=fichas-apostap
+                print("você perdeu tudo!!!")
+                return True
+            else:
+                return False
+        while(loop_point()!=True):
+            loop_point()
+        apostapoint()
