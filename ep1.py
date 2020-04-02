94ac1a1aa623aa1fd59587aefd686522865991bc
Lucas <lucasofa808@gmail.com>
2020-04-02 19:25:12 -0300

    Função para apostar mais ou sair

---
+++ ep1.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+'''função apostar ou não(não terminei)'''
+def apostar():
+    resp=input("\nvocê gostaria de apostar nessa rodada ou sair do jogo?\nDigite'apostar'ou'sair'\n")
+    if (resp =='apostar'):
+        return True
+    else:
+        return False
