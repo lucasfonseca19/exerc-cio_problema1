da9b9a9e1b7c4a8a9a5c226b41e95c96b875deda
maia <maiaf@al.insper.edu.br>
2020-04-02 19:26:28 -0300

    função sorteio dados

--- ep1.py
+++ ep1.py
@@ -3,3 +3,15 @@ import random
 dado1=[1,2,3,4,5,6]
 dado2=[1,2,3,4,5,6]
 fichas=100
+
+
+
+
+
+
+
+def sorteio():
+    x=random.choice(dado1)
+    y=random.choice(dado2)
+    resultado=x+y
+    return resultado
